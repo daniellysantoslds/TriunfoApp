a362a614811c4a9ed8705546b764ad74657be8ca
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-07-11 08:38:39 -0300

    updates

---
+++ TriunfoApp/FeaturedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 04/07/22.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController {
+    @IBOutlet weak var nowplayingCollectionView: UICollectionView!
+    
+    @IBOutlet weak var poupularCollectionView: UICollectionView!
+    
+    @IBOutlet weak var upcomingCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
