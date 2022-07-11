627d4a03b8eb6effd1140f0190fe196fc5a04689
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-07-11 09:21:44 -0300

    updates

---
+++ TriunfoApp/View/NowPlayingCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NowPlayingCollectionViewCell.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 11/07/22.
+//
+
+import UIKit
+
+class NowPlayingCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
---
+++ TriunfoApp/View/PopularCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PopularCollectionViewCell.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 11/07/22.
+//
+
+import UIKit
+
+class PopularCollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet var image: UIImageView!
+    
+    @IBOutlet var title: UILabel!
+    
+}
