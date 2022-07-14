7178038e40e5599bfa19db3867dcccf9ae01e466
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-07-14 09:01:16 -0300

    conectando storyboard com a view

--- TriunfoApp/Controllers/DetailsViewController.swift
+++ TriunfoApp/Controllers/DetailsViewController.swift
@@ -9,16 +9,34 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
+    @IBOutlet var backdropImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var posterImage: UIImageView!
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    
+    
     
     var movie: Movie?
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print(movie)
+        
+        self.title = movie?.title
+        
+        
     }
   
+    
+    
+    
     
 
 }
