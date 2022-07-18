5fb000dc1e29aba732bab714671867a4b27c1fdd
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-07-18 13:03:06 -0300

    up

---
+++ TriunfoApp/View/SearchMoviesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchMoviesViewController.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 18/07/22.
+//
+
+import Foundation
---
+++ TriunfoApp/View/TrendingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TrendingViewController.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 18/07/22.
+//
+
+import Foundation
