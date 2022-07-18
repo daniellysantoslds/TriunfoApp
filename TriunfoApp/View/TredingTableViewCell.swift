e8d6088fac8c6b35725e672e7ef644e7c7006a39
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-07-18 14:06:02 -0300

    pegando

---
+++ TriunfoApp/View/TredingTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TredingTableViewCell.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 18/07/22.
+//
+
+import UIKit
+
+class TredingTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet var posterView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
--- TriunfoApp/View/TrendingViewController.swift
+++ TriunfoApp/View/TrendingViewController.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+
+import UIKit
+
+
+class TrendingViewController: UIViewController{
+    
+    @IBOutlet var trendingTableView: UITableView!
+}
