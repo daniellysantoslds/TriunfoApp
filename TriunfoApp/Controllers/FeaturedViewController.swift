2725436d0db8f7bec2c2fda1d81178c4ae3008e0
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-07-19 15:06:18 -0300

    updates finais

--- TriunfoApp/Controllers/FeaturedViewController.swift
+++ TriunfoApp/Controllers/FeaturedViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class FeaturedViewController: UIViewController, UICollectionViewDataSource {
     
+
     
     var popularMovies: [Movie] = [] //Movie.popularMovies()
     var nowPlayingMovies: [Movie] = []//nowPlayingMovies()
@@ -21,8 +22,42 @@ class FeaturedViewController: UIViewController, UICollectionViewDataSource {
     @IBOutlet weak var popularCollectionView: UICollectionView!
     @IBOutlet weak var upcomingCollectionView: UICollectionView!
     
+    
+    private var imageView: UIImageView {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        imageView.image = UIImage(named: "TRFLOGO")
+        return imageView
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       // view.addSubview(imageView)
+        //view.backgroundColor = .link
+        
+        
+        
+        
+//        func viewDidLayoutSubviews(){
+//           super.viewDidLayoutSubviews()
+//           imageView.center = view.center
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now()0+5, execute: {
+//                self.animate()
+//            })
+//
+//        }
+//
+//        private func animate() {
+//
+//        }
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.\
         popularCollectionView.dataSource = self
         popularCollectionView.delegate = self
@@ -38,7 +73,7 @@ class FeaturedViewController: UIViewController, UICollectionViewDataSource {
         upcomingCollectionView.delegate = self
         
         
-        
+       
         
         
         
@@ -73,3 +108,4 @@ class FeaturedViewController: UIViewController, UICollectionViewDataSource {
         
     }
 }
+
---
+++ TriunfoApp/View/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  TriunfoApp
+//
+//  Created by Danielly Santos Lopes da Silva on 19/07/22.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
